7600641bc3a2fa2824ce7bf67e933352dc1c2a1f
JessieKate <1246505807@qq.com>
2016-12-16 22:53:38 +0800

    add library

---
+++ dialoglibrary/src/main/java/dialogutil/jessie/com/dialoglibrary/load/BaseLoadDialogFragment.java
@@ -0,0 +1,80 @@
+package dialogutil.jessie.com.dialoglibrary.load;
+
+import android.app.Dialog;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import dialogutil.jessie.com.dialoglibrary.R;
+import dialogutil.jessie.com.dialoglibrary.base.BaseDialogFragment;
+
+/**
+ * 创建时间: 03/12/2016
+ * 编写人: JessieKate
+ * 功能描述:载入对话框父类
+ */
+
+public abstract class BaseLoadDialogFragment extends BaseDialogFragment {
+
+    @Override
+    protected Dialog setDialog(View view) {
+        Dialog dlg = new Dialog(getActivity(), R.style.MyDialogBottom);
+        dlg.setCancelable(false);
+        dlg.setCanceledOnTouchOutside(false);
+        dlg.setContentView(view);
+        dlg.getWindow().setBackgroundDrawableResource(R.color.transparent);
+        ProgressBar pb_progress = (ProgressBar) view.findViewById(R.id.pb_progress);
+        TextView load_msg = (TextView) view.findViewById(R.id.load_msg);
+        ImageView dot_loading1 = (ImageView) view.findViewById(R.id.dot_loading1);
+        ImageView dot_loading2 = (ImageView) view.findViewById(R.id.dot_loading2);
+        ImageView dot_loading3 = (ImageView) view.findViewById(R.id.dot_loading3);
+        load_msg.setText(getLoadMsgText());
+        if (getLoadMsgColor() != DEFAULT_COLOR) {
+            load_msg.setTextColor(getResources().getColor(getLoadMsgColor()));
+        }
+        if (getLoadImg() != DEFAULT_COLOR) {
+            pb_progress.setIndeterminateDrawable(getResources().getDrawable(getLoadImg()));
+        }
+        Drawable drawableBlue = getResources().getDrawable(R.drawable.load_cicle_blue);
+        Drawable drawableGray = getResources().getDrawable(R.drawable.load_cicle_gray);
+
+        AnimationDrawable anim1 = new AnimationDrawable();
+        anim1.addFrame(drawableBlue, 300);
+        anim1.addFrame(drawableGray, 300);
+        anim1.addFrame(drawableGray, 300);
+        anim1.setOneShot(false);
+        dot_loading1.setBackgroundDrawable(anim1);
+        anim1.start();
+
+        AnimationDrawable anim2 = new AnimationDrawable();
+        anim2.addFrame(drawableGray, 300);
+        anim2.addFrame(drawableBlue, 300);
+        anim2.addFrame(drawableGray, 300);
+        anim2.setOneShot(false);
+        dot_loading2.setBackgroundDrawable(anim2);
+        anim2.start();
+
+        AnimationDrawable anim3 = new AnimationDrawable();
+        anim3.addFrame(drawableGray, 300);
+        anim3.addFrame(drawableGray, 300);
+        anim3.addFrame(drawableBlue, 300);
+        anim3.setOneShot(false);
+        dot_loading3.setBackgroundDrawable(anim3);
+        anim3.start();
+        return dlg;
+    }
+
+    //获得加载文字颜色
+    protected abstract int getLoadMsgColor();
+
+    //获得加载文字
+    protected abstract String getLoadMsgText();
+
+    //获得加载图片
+    protected abstract int getLoadImg();
+
+
+}
---
+++ dialoglibrary/src/main/java/dialogutil/jessie/com/dialoglibrary/load/LoadDialogFragment.java
@@ -0,0 +1,68 @@
+package dialogutil.jessie.com.dialoglibrary.load;
+
+import android.os.Bundle;
+
+import dialogutil.jessie.com.dialoglibrary.R;
+
+
+/**
+ * 创建时间: 03/12/2016
+ * 编写人: JessieKate
+ * 功能描述:实现加载对话框
+ */
+
+public class LoadDialogFragment extends BaseLoadDialogFragment{
+
+    protected static final String TAG_ARG = "layout";
+    private String loadMsgText="";
+    private int loadMsgColor=DEFAULT_COLOR;
+    private int loadImg=DEFAULT_BG;
+    public static LoadDialogFragment newInstance(){
+        LoadDialogFragment fragment=new LoadDialogFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(TAG_ARG, R.layout.dialog_load);
+        fragment.setArguments(bundle);//把参数传递给该DialogFragment
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            layout = arguments.getInt(TAG_ARG, 0);
+        }
+    }
+
+    public LoadDialogFragment setLoadMsgText(String loadMsgText){
+        this.loadMsgText=loadMsgText;
+        return this;
+    }
+
+    public LoadDialogFragment setLoadMsgColor(int loadMsgColor){
+        this.loadMsgColor=loadMsgColor;
+        return this;
+    }
+
+    public LoadDialogFragment setLoadImg(int loadImg){
+        this.loadImg=loadImg;
+        return this;
+    }
+
+
+    @Override
+    protected int getLoadMsgColor() {
+        return loadMsgColor;
+    }
+
+    @Override
+    protected String getLoadMsgText() {
+        return loadMsgText;
+    }
+
+    @Override
+    protected int getLoadImg() {
+        return loadImg;
+    }
+}
